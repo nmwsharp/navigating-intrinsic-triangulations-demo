2290199979acb9abfeff046f72a2b865790e451e
Nick Sharp <nsharp@cs.cmu.edu>
2019-09-30 13:42:43 +0200

    add command line options

--- README.md
+++ README.md
@@ -3,12 +3,13 @@ Demo code for "Navigating Intrinsic Triangulations". Sharp, Soliman, and Crane. 
 
 **This code is a preview! It is still under construction and there is more to come!**
 
-Our core signpost data structure is implemented in [geometry-central](http://geometry-central.net). This is a simple appliction which loads a mesh, computes an intrinsic triangulation, and visualizes its edges. Notice that the resulting `SignpostIntrinsicTriangulation` class can be used with all geometry routines in geometry-central.
+Our core signpost data structure is implemented in [geometry-central](http://geometry-central.net). This is a simple appliction which loads a mesh, computes an intrinsic triangulation, and visualizes its edges.  Additionally, the code can be invoked from the command line to output data about the intrinsic triangulation in easily-parseable formats.  In this library, the resulting `SignpostIntrinsicTriangulation` class can be used with all geometry routines in geometry-central. 
 
 **Note:** This is not the version of the code which was used to generate the results in the paper; it has been simplified to make it easier to use. If you want to generate precise comparisons against the paper, please contact the authors.
 
 ### Building and running
 
+On unix-like machines, use:
 ```
 git clone --recurse-submodules https://github.com/nmwsharp/navigating-intrinsic-triangulations-demo.git
 cd navigating-intrinsic-triangulations-demo
@@ -18,6 +19,16 @@ make -j4
 ./bin/int_tri /path/to/your/mesh.obj
 ```
 
-This should open a UI window showing your mesh. The intrinsic triangulation is denoted by the colored edge tubes, whose thickness can be adjusted in the settings panel on the left.
+The codebase also builds on Visual Studio 2017 & 2019 (at least), by using CMake to generate a Visual Studio solution file.
 
-The command window in the upper right can be used to flip the intrinsic triangulation to Delaunay, as well as perform Delaunay refinement.
+Running the program open a UI window showing your mesh. The intrinsic triangulation is denoted by the colored edge tubes, whose thickness can be adjusted in the settings panel on the left.
+
+The command window in the upper right can be used to flip the intrinsic triangulation to Delaunay, as well as perform Delaunay refinement. It also has options for outputting to file (see the command line documentation below).
+
+### Command line interface
+
+#### Output formats
+
+Dense matrices are output as an ASCII file where each line is a row of the matrix, which entries separated by spaces. The first line is a comment prefixed by `#`, giving the number of rows and columns in the matrix. Such files can be automatically loaded in many environments (e.g numpy and matlab).
+
+Sparse matrices are output as an ASCII file where each line one entry in the matrix, giving the row, column, and value. The row and column indices are **1-indexed** to make matlab happy. The first line is a comment prefixed by `#`, giving the number of rows and columns in the matrix. These files can be automatically loaded in matlab ([see here](https://www.mathworks.com/help/matlab/ref/spconvert.html)). Writing parsers in other environments should be straightforward.
--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,7 @@ std::unique_ptr<VertexPositionGeometry> geometry;
 std::unique_ptr<SignpostIntrinsicTriangulation> signpostTri;
 
 // Polyscope visualization handle, to quickly add data to the surface
+bool withGUI = true;
 polyscope::SurfaceMesh* psMesh;
 
 // Parameters
@@ -36,7 +37,14 @@ int insertionsMax = -2;
 bool signpostIsDelaunay = true;
 float signpostMinAngleDeg = 0.;
 
+// Output options
+std::string outputPrefix;
+
 void updateTriagulationViz() {
+  if (!withGUI) {
+    return;
+  }
+
   // Update stats
   signpostIsDelaunay = signpostTri->isDelaunay();
   signpostMinAngleDeg = signpostTri->minAngleDegrees();
@@ -67,6 +75,7 @@ void resetTriangulation() {
 
 
 void flipDelaunayTriangulation() {
+  std::cout << "Flipping triangulation to Delaunay" << std::endl;
   signpostTri->flipToDelaunay();
 
   if (!signpostTri->isDelaunay()) {
@@ -78,7 +87,7 @@ void flipDelaunayTriangulation() {
 
 void refineDelaunayTriangulation() {
 
-  if(mesh->hasBoundary()) {
+  if (mesh->hasBoundary()) {
     polyscope::error("refinement only implemented for closed meshes, go bug Nick to finish");
     return;
   }
@@ -87,13 +96,173 @@ void refineDelaunayTriangulation() {
   double sizeParam = useRefineSizeThresh ? refineToSize : std::numeric_limits<double>::infinity();
   size_t maxInsertions = useInsertionsMax ? insertionsMax : INVALID_IND;
 
+  std::cout << "Refining triangulation to Delaunay with:   degreeThresh=" << refineDegreeThresh
+            << " circumradiusThresh=" << refineToSize << " maxInsertions=" << maxInsertions << std::endl;
+
   signpostTri->delaunyRefine(refineDegreeThresh, sizeParam, maxInsertions);
 
   if (!signpostTri->isDelaunay()) {
-    polyscope::warning("Failed to make mesh Delaunay with flips & refinement. Bug Nick to finish porting implementation.");
+    polyscope::warning(
+        "Failed to make mesh Delaunay with flips & refinement. Bug Nick to finish porting implementation.");
   }
 
   updateTriagulationViz();
+}
+
+template <typename T>
+void saveMatrix(std::string filename, SparseMatrix<T>& matrix) {
+
+  // WARNING: this follows matlab convention and thus is 1-indexed
+
+  std::cout << "Writing sparse matrix to: " << filename << std::endl;
+
+  std::ofstream outFile(filename);
+  if (!outFile) {
+    throw std::runtime_error("failed to open output file " + filename);
+  }
+
+  // Write a comment on the first line giving the dimensions
+  outFile << "# sparse " << matrix.rows() << " " << matrix.cols() << std::endl;
+
+  outFile << std::setprecision(16);
+
+  for (int k = 0; k < matrix.outerSize(); ++k) {
+    for (typename SparseMatrix<T>::InnerIterator it(matrix, k); it; ++it) {
+      T val = it.value();
+      size_t iRow = it.row();
+      size_t iCol = it.col();
+
+      outFile << (iRow + 1) << " " << (iCol + 1) << " " << val << std::endl;
+    }
+  }
+
+  outFile.close();
+}
+
+template <typename T>
+void saveMatrix(std::string filename, DenseMatrix<T>& matrix) {
+
+  std::cout << "Writing dense matrix to: " << filename << std::endl;
+
+  std::ofstream outFile(filename);
+  if (!outFile) {
+    throw std::runtime_error("failed to open output file " + filename);
+  }
+
+  // Write a comment on the first line giving the dimensions
+  outFile << "# dense " << matrix.rows() << " " << matrix.cols() << std::endl;
+
+  outFile << std::setprecision(16);
+
+  for (size_t iRow = 0; iRow < (size_t)matrix.rows(); iRow++) {
+    for (size_t iCol = 0; iCol < (size_t)matrix.cols(); iCol++) {
+      T val = matrix(iRow, iCol);
+      outFile << val;
+      if (iCol + 1 != (size_t)matrix.cols()) {
+        outFile << " ";
+      }
+    }
+    outFile << std::endl;
+  }
+
+  outFile.close();
+}
+
+void outputIntrinsicFaces() {
+
+  signpostTri->requireVertexIndices();
+
+  // Assemble Fx3 adjacency matrices vertex indices and edge lengths
+  size_t nV = signpostTri->mesh.nVertices();
+  size_t nF = signpostTri->mesh.nFaces();
+
+  DenseMatrix<double> faceLengths(nF, 3);
+  DenseMatrix<size_t> faceInds(nF, 3);
+
+  size_t iF = 0;
+  for (Face f : signpostTri->mesh.faces()) {
+
+    Halfedge he = f.halfedge();
+    for (int v = 0; v < 3; v++) {
+
+      Vertex vA = he.vertex();
+      Vertex vB = he.twin().vertex();
+      size_t indA = signpostTri->vertexIndices[vA];
+      size_t indB = signpostTri->vertexIndices[vB];
+      Edge e = he.edge();
+      double l = signpostTri->intrinsicEdgeLengths[e];
+
+      faceLengths(iF, v) = l;
+      faceInds(iF, v) = indA;
+
+      he = he.next();
+    }
+
+    iF++;
+  }
+
+  saveMatrix("faceInds.dmat", faceInds);
+  saveMatrix("faceLengths.dmat", faceLengths);
+}
+
+void outputVertexPositions() {
+
+  signpostTri->requireVertexIndices();
+
+  size_t nV = signpostTri->mesh.nVertices();
+  DenseMatrix<double> vertexPositions(nV, 3);
+
+  VertexData<Vector3> intrinsicPositions = signpostTri->sampleAtInput(geometry->inputVertexPositions);
+
+  size_t iV = 0;
+  for (Vertex v : signpostTri->mesh.vertices()) {
+    Vector3 p = intrinsicPositions[v];
+    vertexPositions(iV, 0) = p.x;
+    vertexPositions(iV, 1) = p.y;
+    vertexPositions(iV, 2) = p.z;
+    iV++;
+  }
+
+  saveMatrix("vertexPositions.dmat", vertexPositions);
+}
+
+void outputLaplaceMat() {
+  signpostTri->requireCotanLaplacian();
+  saveMatrix("laplace.spmat", signpostTri->cotanLaplacian);
+}
+
+void outputInterpolatMat() {
+  signpostTri->requireVertexIndices();
+
+  // Assemble Fx3 adjacency matrices vertex indices and edge lengths
+  size_t nV = signpostTri->mesh.nVertices();
+
+  SparseMatrix<double> interpMat(nV, nV);
+  std::vector<Eigen::Triplet<double>> triplets;
+
+  size_t iV = 0;
+  for (Vertex v : signpostTri->mesh.vertices()) {
+    SurfacePoint p = signpostTri->vertexLocations[v];
+    p = p.inSomeFace();
+
+    Face f = p.face;
+
+    int j = 0;
+    for (Vertex n : f.adjacentVertices()) {
+      size_t jV = signpostTri->vertexIndices[n];
+      double w = p.faceCoords[j];
+      if (w > 0) {
+        triplets.emplace_back(iV, jV, w);
+      }
+      j++;
+    }
+    iV++;
+  }
+
+  interpMat.setFromTriplets(triplets.begin(), triplets.end());
+  interpMat.makeCompressed();
+
+  saveMatrix("interpolate.spmat", interpMat);
 }
 
 void myCallback() {
@@ -119,7 +288,6 @@ void myCallback() {
     ImGui::TreePop();
   }
 
-
   if (ImGui::TreeNode("Delaunay refinement")) {
     ImGui::InputFloat("degree threshold", &refineDegreeThresh);
 
@@ -139,14 +307,45 @@ void myCallback() {
     ImGui::TreePop();
   }
 
+  if (ImGui::TreeNode("Output")) {
+
+    if (ImGui::Button("intrinsic faces")) outputIntrinsicFaces();
+    if (ImGui::Button("vertex positions")) outputVertexPositions();
+    if (ImGui::Button("Laplace matrix")) outputLaplaceMat();
+    if (ImGui::Button("interpolate matrix")) outputInterpolatMat();
+
+    ImGui::TreePop();
+  }
+
   ImGui::PopItemWidth();
 }
 
 int main(int argc, char** argv) {
 
   // Configure the argument parser
-  args::ArgumentParser parser("A demo of Naavigating Intrinsic Triangulations");
+  // clang-format off
+  args::ArgumentParser parser("A demo of Navigating Intrinsic Triangulations");
+  args::HelpFlag help(parser, "help", "Display this help message", {'h', "help"});
   args::Positional<std::string> inputFilename(parser, "mesh", "A .obj or .ply mesh file.");
+
+  args::Group triangulation(parser, "triangulation");
+  args::Flag flipDelaunay(triangulation, "flipDelaunay", "Flip edges to make the mesh intrinsic Delaunay", {"flipDelaunay"});
+  args::Flag refineDelaunay(triangulation, "refineDelaunay", "Refine and flip edges to make the mesh intrinsic Delaunay and satisfy angle/size bounds", {"refineDelaunay"});
+  args::ValueFlag<double> refineAngle(triangulation, "refineAngle", "Minimum angle threshold (in degrees). Default: 25.", {"refineAngle"}, 25.);
+  args::ValueFlag<double> refineSizeCircum(triangulation, "refineSizeCircum", "Maximum triangle size, set by specifying the circumradius. Default: inf", {"refineSizeCircum"}, std::numeric_limits<double>::infinity());
+  args::ValueFlag<int> refineMaxInsertions(triangulation, "refineMaxInsertions", 
+      "Maximum number of insertions during refinement. Use 0 for no max, or negative values to scale by number of vertices. Default: 10 * nVerts", 
+      {"refineMaxInsertions"}, -10);
+
+  args::Group output(parser, "ouput");
+  args::Flag noGUI(output, "noGUI", "exit after processing and do not open the GUI", {"noGUI"});
+  args::ValueFlag<std::string> outputPrefixArg(output, "outputPrefix", "Prefix to prepend to all output file paths. Default: intrinsic_", {"outputPrefix"}, "intrinsic_");
+  args::Flag intrinsicFaces(output, "edgeLengths", "write the face information for the intrinsic triangulation. name: 'faceInds.dmat, faceLengths.dmat'", {"intrinsicFaces"});
+  args::Flag vertexPositions(output, "vertexPositions", "write the vertex positions for the intrinsic triangulation. name: 'vertexPositions.dmat'", {"vertexPositions"});
+  args::Flag laplaceMat(output, "laplaceMat", "write the Laplace-Beltrami matrix for the triangulation. name: 'laplace.spmat'", {"laplaceMat"});
+  args::Flag interpolateMat(output, "interpolateMat", "write the matrix which expresses data on the intrinsic vertices as a linear combination of the input vertices. name: 'interpolate.mat'", {"interpolateMat"});
+  // clang-format on
+
 
   // Parse args
   try {
@@ -162,61 +361,80 @@ int main(int argc, char** argv) {
 
   // Make sure a mesh name was given
   if (!inputFilename) {
-    std::cerr << "Please specify a mesh file as argument" << std::endl;
+    std::cout << parser;
     return EXIT_FAILURE;
   }
 
-  // Initialize polyscope
-  polyscope::init();
+  // Set options
+  withGUI = !noGUI;
 
-  // Set the callback function
-  polyscope::state::userCallback = myCallback;
+  refineDegreeThresh = args::get(refineAngle);
+  refineToSize = args::get(refineSizeCircum);
+  useRefineSizeThresh = refineToSize < std::numeric_limits<double>::infinity();
+  insertionsMax = args::get(refineMaxInsertions);
+  useInsertionsMax = insertionsMax != 0;
+  outputPrefix = args::get(outputPrefixArg);
 
   // Load mesh
   std::tie(mesh, geometry) = loadMesh(args::get(inputFilename));
 
-  // Register the mesh with polyscope
-  psMesh = polyscope::registerSurfaceMesh(polyscope::guessNiceNameFromPath(args::get(inputFilename)),
-                                          geometry->inputVertexPositions, mesh->getFaceVertexList(),
-                                          polyscopePermutations(*mesh));
-
-
-  // Set vertex tangent spaces
-  geometry->requireVertexTangentBasis();
-  VertexData<Vector3> vBasisX(*mesh);
-  for (Vertex v : mesh->vertices()) {
-    vBasisX[v] = geometry->vertexTangentBasis[v][0];
+  // Sale max insertions by number of vertices if needed
+  if (insertionsMax < 0) {
+    insertionsMax *= -mesh->nVertices();
   }
-  polyscope::getSurfaceMesh()->setVertexTangentBasisX(vBasisX);
 
-  // Set face tangent spaces
-  geometry->requireFaceTangentBasis();
-  FaceData<Vector3> fBasisX(*mesh);
-  for (Face f : mesh->faces()) {
-    fBasisX[f] = geometry->faceTangentBasis[f][0];
+  if (withGUI) {
+
+    // Initialize polyscope
+    polyscope::init();
+
+    // Set the callback function
+    polyscope::state::userCallback = myCallback;
+
+    // Register the mesh with polyscope
+    psMesh = polyscope::registerSurfaceMesh(polyscope::guessNiceNameFromPath(args::get(inputFilename)),
+                                            geometry->inputVertexPositions, mesh->getFaceVertexList(),
+                                            polyscopePermutations(*mesh));
+
+
+    // Set vertex tangent spaces
+    geometry->requireVertexTangentBasis();
+    VertexData<Vector3> vBasisX(*mesh);
+    for (Vertex v : mesh->vertices()) {
+      vBasisX[v] = geometry->vertexTangentBasis[v][0];
+    }
+    polyscope::getSurfaceMesh()->setVertexTangentBasisX(vBasisX);
+
+    // Set face tangent spaces
+    geometry->requireFaceTangentBasis();
+    FaceData<Vector3> fBasisX(*mesh);
+    for (Face f : mesh->faces()) {
+      fBasisX[f] = geometry->faceTangentBasis[f][0];
+    }
+    polyscope::getSurfaceMesh()->setFaceTangentBasisX(fBasisX);
+
+    // Nice defaults
+    psMesh->edgeWidth = 1.0;
   }
-  polyscope::getSurfaceMesh()->setFaceTangentBasisX(fBasisX);
-
-  // Nice defaults
-  psMesh->edgeWidth = 1.0;
-
-  // Set initial values for parameters
-  double surfaceArea = 0.;
-  geometry->requireFaceAreas();
-  for (Face f : mesh->faces()) {
-    surfaceArea += geometry->faceAreas[f];
-  }
-  double meanFaceArea = surfaceArea / mesh->nFaces();
-  refineToSize = std::sqrt(meanFaceArea);
-  insertionsMax = 5 * mesh->nVertices();
-  useInsertionsMax = true;
 
 
   // Initialize triangulation
   resetTriangulation();
 
+  // Perform any operations requested
+  if (flipDelaunay) flipDelaunayTriangulation();
+  if (refineDelaunay) refineDelaunayTriangulation();
+
+  // Generate any outputs
+  if (intrinsicFaces) outputIntrinsicFaces();
+  if (vertexPositions) outputVertexPositions();
+  if (laplaceMat) outputLaplaceMat();
+  if (interpolateMat) outputInterpolatMat();
+
   // Give control to the polyscope gui
-  polyscope::show();
+  if (withGUI) {
+    polyscope::show();
+  }
 
   return EXIT_SUCCESS;
 }
